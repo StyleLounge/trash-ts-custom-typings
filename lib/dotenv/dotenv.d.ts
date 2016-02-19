b1b4a7373f4a43ce923d32ea8c91ea4efe79ac10
Alexander Thiel <alexander@homedesignsolutions.de>
2016-02-19 23:44:47 +0100

    added dotenv

---
+++ lib/dotenv/dotenv.d.ts
@@ -0,0 +1,20 @@
+/// <reference path="../../typings/main.d.ts" />
+
+declare var dotenv:dotenv.IDotEnvStatic;
+
+declare module dotenv {
+
+    interface IConfigOptions {
+        encoding?: string;
+        silent?: boolean;
+        path?: string;
+    }
+    interface IDotEnvStatic {
+        config(options?:IConfigOptions): any
+        parse(buffer:any) : any
+    }
+}
+
+declare module "dotenv" {
+    export = dotenv;
+}
