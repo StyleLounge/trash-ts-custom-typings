74a07ff53682e0e42c847f0fe6e4d2fb36c91e20
Alexander Thiel <alexander@homedesignsolutions.de>
2016-03-27 20:10:07 +0200

    improved aglio

--- lib/aglio/aglio.d.ts
+++ lib/aglio/aglio.d.ts
@@ -5,7 +5,13 @@
  * Methods used by StyleLounge are added as needed. DO NOT RELY ON THIS TYPINGS' ACCURACY!
  */
 
-declare var aglio:any;
+
+declare var aglio:IAglioStatic;
+
+interface IAglioStatic {
+    render (input:string, options:any, callback:(error: Error, html: string, warnings: any) => void):void;
+    renderFile (input:string, output:string, options:any, callback:(error: Error, warnings: any) => void):void;
+}
 
 declare module "aglio" {
     export = aglio;
