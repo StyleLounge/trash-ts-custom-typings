deb438ce010f830f0fb492e1298d66664c514d3c
Alexander Thiel <alexander@homedesignsolutions.de>
2016-05-20 13:43:41 +0200

    made fast-csv params optional

--- lib/fast-csv/fast-csv.d.ts
+++ lib/fast-csv/fast-csv.d.ts
@@ -19,31 +19,31 @@ declare module "fast-csv" {
          * Ensure that data events have an object emitted rather than the stringified version set to false to have a stringified buffer.
          * Defaults to true
          */
-        objectMode: boolean;
+        objectMode?: boolean;
 
         /**
          * Set to true if you expect the first line of your CSV to contain headers, alternatly you can specify an array of headers to use. You can also specify a sparse array to omit some of the columns.
          * Defaults to false
          */
-        headers: boolean;
+        headers?: boolean;
 
         /**
          * If you wish to ignore empty rows.
          * Defaults to false
          */
-        ignoreEmpty: boolean;
+        ignoreEmpty?: boolean;
 
         /**
          * If you want to discard columns that do not map to a header.
          * Defaults to false
          */
-        discardUnmappedColumns: boolean
+        discardUnmappedColumns?: boolean
 
         /**
          * If you want to consider empty lines/lines with too few fields as errors - Only to be used with headers=true
          * Defaults to false
          */
-        strictColumnHandling: boolean;
+        strictColumnHandling?: boolean;
 
         /**
          * If your data uses an alternate delimiter such as ; or \t.
@@ -51,7 +51,7 @@ declare module "fast-csv" {
          * quote='"': The character to use to escape values that contain a delimiter. If you set to null then all quoting will be ignored
          * Defaults to ','
          */
-        delimiter: string;
+        delimiter?: string;
 
         /**
          * The character to use when escaping a value that is quoted and contains a quote character.
@@ -59,31 +59,31 @@ declare module "fast-csv" {
          * The following are options for parsing only.
          * Defaults to '"'
          */
-        escape: string;
+        escape?: string;
 
         /**
          * If you want to trim all values parsed set to true.
          * Defaults to false
          */
-        trim: boolean;
+        trim?: boolean;
 
         /**
          * If you want to right trim all values parsed set to true.
          * Defaults to false
          */
-        rtrim: boolean;
+        rtrim?: boolean;
 
         /**
          * If you want to left trim all values parsed set to true.
          * Defaults to false
          */
-        ltrim: boolean;
+        ltrim?: boolean;
 
         /**
          * If your CSV contains comments you can use this option to ignore lines that begin with the specified character (e.g. #).
          * Defaults to false
          */
-        comment: boolean;
+        comment?: boolean;
     }
 
     export = fastCsv;
