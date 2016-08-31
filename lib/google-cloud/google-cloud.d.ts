cfef0196e0b8255263759d6045829d2912f47cf0
André König <andre.koenig@stylelounge.de>
2016-08-31 15:49:37 +0200

    feat: switch to `google-cloud`

---
+++ lib/google-cloud/google-cloud.d.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../typings/main.d.ts" />
+
+/*
+ * ATTENTION: This is only a stub and does not reflect the types that are actually available!
+ * Methods used by StyleLounge are added as needed. DO NOT RELY ON THIS TYPINGS' ACCURACY!
+ */
+
+declare var gcloud:any;
+
+declare module "google-cloud" {
+    export = gcloud;
+}
